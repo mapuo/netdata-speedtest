9abfab58d314325de9466918029b3a9c54470596
Pavel Nakonechnyi <pavel@gremwell.com>
2019-10-13 19:18:01 +0200

    making plugin work asynchronously
    The approach of blocking execution of a long task contradicts with
    'netdata' way of work: i.e. it is designed (*sigh*) to launch single
    thread for each charts.d orchestrator which runs each collector one by
    one. This leads to blocking of whole charts.d instance and lack of data
    as a result.

--- speedtest.chart.sh
+++ speedtest.chart.sh
@@ -4,9 +4,12 @@
 # Requires installed speedtest.com cli: `pip install speedtest-cli`
 speedtest_update_every=60
 speedtest_priority=100
+speedtest_tmpfile="/tmp/speedtest_out.tmp"
 
 speedtest_check() {
     require_cmd speedtest || return 1
+    # flushing temporary file content to something predictable
+    echo > $speedtest_tmpfile
     return 0
 }
 
@@ -23,13 +26,17 @@ EOF
 }
 
 speedtest_update() {
-    # do all the work to collect / calculate the values
-    # for each dimension
-    # remember: KEEP IT SIMPLE AND SHORT
-    # Get the up and down speed. Parse them into separate values, and drop the Mbps.
-    speedtest_output=$(speedtest --single --csv)
-    down=$(echo "$speedtest_output" | cut -d ',' -f 7 | cut -d '.' -f 1)
-    up=$(echo "$speedtest_output" | cut -d ',' -f 8 | cut -d '.' -f 1)
+    # get the up and down speed from the previously executed . Parse them into separate values, and drop the Mbps.
+    speedtest_output=$(cat $speedtest_tmpfile)
+    # collect speed test results in background
+    speedtest --single --csv > $speedtest_tmpfile &
+
+    down=0
+    up=0
+    if [ -n "$speedtest_output" ]; then
+        down=$(echo "$speedtest_output" | cut -d ',' -f 7 | cut -d '.' -f 1)
+        up=$(echo "$speedtest_output" | cut -d ',' -f 8 | cut -d '.' -f 1)
+    fi
 
     # write the result of the work.
     cat <<VALUESEOF
