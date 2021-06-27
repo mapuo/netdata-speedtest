31d1b87c128070fcd21b63393272194a348e5e82
Mario Georgiev <mario.georgiev@gmail.com>
2021-06-27 14:57:28 +0300

    Add config to use specific server
    There is now a configuration option to use specific server.
    It accepts server Id or server Name.
    There is a fallback if the command cannot run because of missing/unreachable server.

--- speedtest.chart.sh
+++ speedtest.chart.sh
@@ -5,10 +5,16 @@
 speedtest_update_every=60
 speedtest_priority=100
 speedtest_tmpfile="/tmp/speedtest_out.tmp"
+speedtest_server=
 
 speedtest_check() {
     require_cmd speedtest || return 1
     require_cmd bc || return 1
+    require_cmd xargs || return 1
+    # Find the server from the list
+    server=$(speedtest --list | grep -m 1 "$speedtest_server" | cut -d ')' -f 1 | xargs)
+    # Set the server id
+    speedtest_server=$server
     # flushing temporary file content to something predictable
     echo > $speedtest_tmpfile
     return 0
@@ -32,7 +38,8 @@ speedtest_update() {
     # get the up and down speed from the previously executed . Parse them into separate values, and drop the Mbps.
     speedtest_output=$(cat $speedtest_tmpfile)
     # collect speed test results in background
-    speedtest --single --csv > $speedtest_tmpfile &
+    server=${speedtest_server:+" --server $speedtest_server"}
+    (speedtest --single --csv $server 2> /dev/null || speedtest --single --csv) | echo $(cat -) > $speedtest_tmpfile &
 
     down=0
     up=0
